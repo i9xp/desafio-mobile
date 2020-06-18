6ce999d834a1b2fa1744306c99084a06c56389cc
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-18 18:30:53 -0300

    reestrutaracao dos folders para facilitar a leitura

---
+++ teste_app/lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:testeapp/src/pages/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
