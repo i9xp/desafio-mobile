591c5af77f5e3278b6ed5d63681eab2f3679e521
JoaoRicardi <joao_ricardi@hotmail.com>
2020-06-19 10:37:45 -0300

    on Tap no produto para fazer a navegagacao

---
+++ teste_app/lib/src/pages/detail/ProductDetail.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- teste_app/lib/src/widgets/listitem/ProductsListItem.dart
+++ teste_app/lib/src/widgets/listitem/ProductsListItem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testeapp/src/pages/detail/ProductDetail.dart';
 import 'package:testeapp/src/values/colors.dart' as colors;
 import 'package:testeapp/src/widgets/Text.dart';
 
@@ -12,57 +13,68 @@ class ProductListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 111,
-      height: 145,
-      decoration: BoxDecoration(
-          color: colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(24))
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 8,
-              left: 8,
-              right: 8
-            ),
-            child: Container(
-              width: 111,
-              height: 95,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(
-                        asset,
-                      )
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(24),
-                    topLeft: Radius.circular(24)
-                  )
+    return InkWell(
+      onTap:() => navigateToDetails(context: context),
+      child: Container(
+        width: 111,
+        height: 145,
+        decoration: BoxDecoration(
+            color: colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(24))
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 8,
+                left: 8,
+                right: 8
+              ),
+              child: Container(
+                width: 111,
+                height: 95,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                          asset,
+                        )
+                    ),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(24),
+                      topLeft: Radius.circular(24)
+                    )
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left:8.0),
-            child: CustomText(
-              text: text,
-              color: colors.grayDark,
+            Padding(
+              padding: const EdgeInsets.only(left:8.0),
+              child: CustomText(
+                text: text,
+                color: colors.grayDark,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left:8.0),
-            child: CustomText(
-              size: TextSize.SMALL,
-              color: colors.grayDark,
-              text: r"R$"+ "$value",
+            Padding(
+              padding: const EdgeInsets.only(left:8.0),
+              child: CustomText(
+                size: TextSize.SMALL,
+                color: colors.grayDark,
+                text: r"R$"+ "$value",
+              ),
             ),
-          ),
 
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  void navigateToDetails({BuildContext context}){
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context){
+        return ProductDetail();
+      }
+    ));
   }
 }
