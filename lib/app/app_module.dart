9af2db5cea1ea982528f246cf89bb7d53d6c4894
JP <joaovirgili@Mac-mini-de-JP.local>
2020-07-07 18:59:04 -0300

    Estrutura do projeto montada

---
+++ lib/app/app_module.dart
@@ -0,0 +1,22 @@
+import 'app_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:i9xp/app/app_widget.dart';
+import 'package:i9xp/app/modules/home/home_module.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => AppController()),
+      ];
+
+  @override
+  List<Router> get routers => [
+        Router(Modular.initialRoute, module: HomeModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
