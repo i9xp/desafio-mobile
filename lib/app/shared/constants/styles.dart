bb905f68368ac6ea862ab2d39b910421e84298da
JP <joaovirgili@Mac-mini-de-JP.local>
2020-07-07 19:35:19 -0300

    Adicionado os text styles da HomePage

---
+++ lib/app/shared/constants/styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:i9xp/app/shared/constants/colors.dart';
+
+class AppStyles {
+  // ignore: non_constant_identifier_names
+  static final SECTION_TITLE = TextStyle(
+    fontSize: 30.sp,
+    color: AppColors.ON_BACKGROUND,
+    fontWeight: FontWeight.bold,
+  );
+
+  // ignore: non_constant_identifier_names
+  static final CATEGORY_ITEM = TextStyle(
+    fontSize: 15.sp,
+    color: AppColors.ON_BACKGROUND,
+    fontWeight: FontWeight.bold,
+  );
+
+  // ignore: non_constant_identifier_names
+  static final PRODUCT_TITLE = TextStyle(
+    fontSize: 12.sp,
+    color: AppColors.ON_CARD,
+    fontWeight: FontWeight.w300,
+  );
+
+  // ignore: non_constant_identifier_names
+  static final PRODUCT_PRICE = TextStyle(
+    fontSize: 10.sp,
+    color: AppColors.ON_CARD,
+    fontWeight: FontWeight.bold,
+  );
+
+  // ignore: non_constant_identifier_names
+  static final TABBAR_ITEM = TextStyle(
+    fontSize: 11.sp,
+    color: AppColors.ON_BACKGROUND,
+  );
+
+  // ignore: non_constant_identifier_names
+  static final BADGE_NUMBER = TextStyle(
+    fontSize: 10.sp,
+    color: AppColors.ON_BACKGROUND,
+    fontWeight: FontWeight.bold,
+  );
+}
