2504e1ef49a099b70cd2f2791c6205f2a7ff2315
JP <joaovirgili@Mac-mini-de-JP.local>
2020-07-07 19:40:38 -0300

    Adicionada cor de fundo da HomePage

--- lib/app/shared/constants/colors.dart
+++ lib/app/shared/constants/colors.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // TEXT
+  // ignore: non_constant_identifier_names
+  static final HOME_BACKGROUND_A = Color(0xff2B3340);
+
   // ignore: non_constant_identifier_names
   static final ON_BACKGROUND = Colors.white;
 
-  // TABBAR
   // ignore: non_constant_identifier_names
   static final TABBAR_COLOR = Color(0xff2E3746);
   // ignore: non_constant_identifier_names
   static final SELECTED_TAB = Color(0xffFFC600);
 
-  // CARD
   // ignore: non_constant_identifier_names
   static final ON_CARD = Color(0xff515C6F);
   // ignore: non_constant_identifier_names
   static final CARD_BACKGROUND = Colors.white;
 
-  // BADGE
   // ignore: non_constant_identifier_names
   static final BAGDE_BACKGROUND = Color(0xffF9C705);
   // ignore: non_constant_identifier_names
