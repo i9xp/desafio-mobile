dfc65d4e6ef9281457161c0876429a2ca2e40fd0
JP <joaovirgili@Mac-mini-de-JP.local>
2020-07-07 19:46:54 -0300

    Adicionado arquivo de constantes pros assets

---
+++ lib/app/shared/constants/assets.dart
@@ -0,0 +1,52 @@
+class AppAssets {
+  // ignore: non_constant_identifier_names
+  static final APPAREL = "assets/img/home/apparel.png";
+
+  // ignore: non_constant_identifier_names
+  static final BACKPACK = "assets/img/home/backpack.png";
+
+  // ignore: non_constant_identifier_names
+  static final BANNER_1 = "assets/img/home/banner_1.png";
+
+  // ignore: non_constant_identifier_names
+  static final BANNER_2 = "assets/img/home/banner_2.png";
+
+  // ignore: non_constant_identifier_names
+  static final BEAUTY = "assets/img/home/beauty.png";
+
+  // ignore: non_constant_identifier_names
+  static final CART = "assets/img/home/cart.png";
+
+  // ignore: non_constant_identifier_names
+  static final GRUPO = "assets/img/home/grupo.png";
+
+  // ignore: non_constant_identifier_names
+  static final HOME = "assets/img/home/home.png";
+
+  // ignore: non_constant_identifier_names
+  static final MESSAGES = "assets/img/home/messages.png";
+
+  // ignore: non_constant_identifier_names
+  static final MORE = "assets/img/home/more.png";
+
+  // ignore: non_constant_identifier_names
+  static final NOTIFICATIONS = "assets/img/home/notifications.png";
+
+  // ignore: non_constant_identifier_names
+  static final PROFILE = "assets/img/home/profile.png";
+
+  // ignore: non_constant_identifier_names
+  static final SCARF = "assets/img/home/scarf.png";
+
+  // ignore: non_constant_identifier_names
+  static final SEARCH = "assets/img/home/search.png";
+
+  // ignore: non_constant_identifier_names
+  static final SEE_ALL = "assets/img/home/see_all.png";
+
+  // ignore: non_constant_identifier_names
+  static final SHOES = "assets/img/home/shoes.png";
+
+  // ignore: non_constant_identifier_names
+  static final WOMAN_SHOES = "assets/img/home/woman_shoes.png";
+}
