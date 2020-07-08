bf611fddc038dc9718ecdbf41013890fe0aecb6e
Gustavo <gustavo10ssouza@gmail.com>
2020-07-08 14:36:36 -0300

    init project

---
+++ challenge/lib/app/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(
+        width: width,
+        height: height,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
