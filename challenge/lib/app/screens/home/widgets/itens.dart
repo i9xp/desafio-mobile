80580f19216841fa69b84c49052a3e227039a586
Gustavo <gustavo10ssouza@gmail.com>
2020-07-10 02:07:27 -0300

    custom widget itens

--- challenge/lib/app/screens/home/widgets/itens.dart
+++ challenge/lib/app/screens/home/widgets/itens.dart
@@ -2,10 +2,16 @@ import 'package:challenge/app/screens/product/product.dart';
 import 'package:flutter/material.dart';
 
 class ItensWidget extends StatelessWidget {
+  final List<String> title = ['Ankle Boots', 'Backpack', 'Red Scorf'];
+  final List<String> image = [
+    'assets/img/ankle.png',
+    'assets/img/backpack.png',
+    'assets/img/redscorf.png',
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 6,
+      itemCount: 3,
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
         return GestureDetector(
@@ -30,8 +36,10 @@ class ItensWidget extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Image.asset(
-                      'assets/img/backpack.png',
+                    Expanded(
+                      child: Image.asset(
+                        image[index],
+                      ),
                     ),
                     SizedBox(
                       height: 10,
@@ -40,7 +48,7 @@ class ItensWidget extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text(
-                          'BackPack',
+                          title[index],
                           style:
                               TextStyle(color: Color(0xff515C6F), fontSize: 12),
                         ),
