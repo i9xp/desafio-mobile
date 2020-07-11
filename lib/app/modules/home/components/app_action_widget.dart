a2e366b96ac7c6bf6e9e109b8c474c0c5ca633bf
JP <joaovirgili@Mac-mini-de-JP.local>
2020-07-10 23:04:18 -0300

    Ajustado o AppAction

--- lib/app/modules/home/components/app_action_widget.dart
+++ lib/app/modules/home/components/app_action_widget.dart
@@ -31,37 +31,38 @@ class _AppActionState extends State<AppAction> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 25,
-      height: 25,
-      padding: EdgeInsets.symmetric(vertical: 16),
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: Image.asset(asset),
-          ),
-          if (hasBadge)
+    return Center(
+      child: Container(
+        width: 25,
+        height: 25,
+        child: Stack(
+          children: [
             Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                width: 15,
-                height: 15,
-                decoration: BoxDecoration(
-                  color: AppColors.BAGDE_BACKGROUND,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Text(
-                    "${widget.number}",
-                    style: ten
-                        ? AppStyles.BADGE_NUMBER_TEN
-                        : AppStyles.BADGE_NUMBER,
+              alignment: Alignment.topRight,
+              child: Image.asset(asset),
+            ),
+            if (hasBadge)
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                    color: AppColors.BAGDE_BACKGROUND,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Text(
+                      "${widget.number}",
+                      style: ten
+                          ? AppStyles.BADGE_NUMBER_TEN
+                          : AppStyles.BADGE_NUMBER,
+                    ),
                   ),
                 ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
