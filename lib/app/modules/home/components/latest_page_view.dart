63b4a046afa105f9d8e31d2115b6445c41fa8dfe
JP <joaovirgili@Mac-mini-de-JP.local>
2020-07-12 01:46:04 -0300

    Ajustado layout da pageview de latest

--- lib/app/modules/home/components/latest_page_view.dart
+++ lib/app/modules/home/components/latest_page_view.dart
@@ -10,12 +10,41 @@ class LatestPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 235.h,
+      height: 184.h,
       child: PageView(
+        controller: PageController(
+          viewportFraction: 0.9,
+          initialPage: 0,
+        ),
         children: [
-          Image.asset(AppAssets.BANNER_1),
-          Image.asset(AppAssets.BANNER_2),
+          LatestImage(image: AppAssets.BANNER_1),
+          LatestImage(image: AppAssets.BANNER_2),
         ],
+      ),
+    );
+  }
+}
+
+class LatestImage extends StatelessWidget {
+  const LatestImage({
+    Key key,
+    this.image,
+  }) : super(key: key);
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(image),
+          ),
+        ),
       ),
     );
   }
